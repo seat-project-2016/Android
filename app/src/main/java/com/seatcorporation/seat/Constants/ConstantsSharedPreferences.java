f4ce61a13f12337dad48682272b76153919ab94f
Seat <seat.project.2016>
2016-09-27 22:07:51 +0530

    added missing files

---
+++ app/src/main/java/com/seatcorporation/seat/Constants/ConstantsSharedPreferences.java
@@ -0,0 +1,12 @@
+package com.seatcorporation.seat.Constants;
+
+/**
+ * Created by Devrath on 27/09/16.
+ */
+
+public class ConstantsSharedPreferences {
+
+    public static final String BOOL_IS_USER_REGISTERED = "isUserRegistered";
+
+
+}
